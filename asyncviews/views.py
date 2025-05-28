82918126ef435e4ab0b8459b7abb8b874e0a51b7
Matheus <matheus271112@gmail.com>
2025-05-28 16:56:55 -0300

    Atividade EBAC

--- asyncviews/views.py
+++ asyncviews/views.py
@@ -3,7 +3,7 @@ import httpx
 
 
 async def http_call_async( request ):
-    for num in range(1, 11):
+    for num in range(1, 12):
         await asyncio.sleep(1)
         print(num)
     async with httpx.AsyncClient() as client:
